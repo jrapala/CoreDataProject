66670ee60148a419cc3a8a1c2dad931fe7a67f21
Juliette Rapala <jrapala@gmail.com>
2020-11-23 12:30:00 -0600

    feat: add Singer entity and helper methods

---
+++ CoreDataProject/Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Juliette Rapala on 11/23/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
